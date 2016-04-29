c557c7e63ce9d46df7fe01383341ccb29ccaa59f
ricardo-zorio <zorioricardo@gmail.com>
2016-04-29 16:10:44 -0300

    added main
    added main.c for testing

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello world!\n");
+    while (1++)
+    {
+        printf("Hello world!\n");
+    }
+
+    printf("Hello world!\n");
+    return 0;
+}
+
